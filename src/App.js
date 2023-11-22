fae0bca5168da6b318e762519f9e03d66a2df894
menth0s <marslite@pm.me>
2023-11-21 20:46:49 -0500

    Fixed formatting

--- src/App.js
+++ src/App.js
@@ -12,13 +12,7 @@ function App() {
     <div>
       <Navbar/>
 
-{/* 
-      <div className="alert alert-success" role="alert">
-      <h4 className="alert-heading">Well done!</h4>
-    <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-    <hr/>
-    <p className="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
-    </div> */}
+
 
 <div className="card text-bg-light mb-3" style={{"max-width": "18rem;"}}>
   <div className="card-header"><b>Multiblock Beta</b></div>
@@ -38,27 +32,6 @@ function App() {
 
   
   </div>
-
-
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-
-
 
   );
 }
--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -21,45 +21,55 @@ const HomePage = () => {
 
   return (
     <div>
-       <div className="web-container">
+      <div className='site-wrapper'>
+        <div className='web-wrap'>
+        <div className="web-container">
+
+
+                      <h1>Router App - Domain Blacklist</h1>
+                      <form onSubmit={handleSubmit}>
+                      <div className="input-group">
+                        <label>Domain 1:</label>
+                        <input 
+                          type="text" 
+                          value={domainOne} 
+                          onChange={(e) => setDomainOne(e.target.value)} 
+                        />
+                      </div>
+                      <div className="input-group">
+                        <label>Domain 2:</label>
+                        <input 
+                          type="text" 
+                          value={domainTwo} 
+                          onChange={(e) => setDomainTwo(e.target.value)} 
+                        />
+                      </div>
+                      <div className="input-group">
+                        <label>Upload File:</label>
+                        <input 
+                          type="file" 
+                          onChange={handleFileChange} 
+                          accept=".txt"
+                        />
+                      </div>
+                      <button type="submit" className='btn btn-danger mt-2 btn-d'>
+                        <b>Submit</b>
+                        </button>
+                      </form>
+
+                      </div>
+
+        </div>
+  <Footer/>
 
 
 
+      </div>
 
-<h1>Router App - Domain Blacklist</h1>
-<form onSubmit={handleSubmit}>
-  <div className="input-group">
-    <label>Domain 1:</label>
-    <input 
-      type="text" 
-      value={domainOne} 
-      onChange={(e) => setDomainOne(e.target.value)} 
-    />
-  </div>
-  <div className="input-group">
-    <label>Domain 2:</label>
-    <input 
-      type="text" 
-      value={domainTwo} 
-      onChange={(e) => setDomainTwo(e.target.value)} 
-    />
-  </div>
-  <div className="input-group">
-    <label>Upload File:</label>
-    <input 
-      type="file" 
-      onChange={handleFileChange} 
-      accept=".txt"
-    />
-  </div>
-  <button type="submit" className='btn btn-danger mt-2 btn-d'>
-    <b>Submit</b>
-    </button>
-</form>
+
+
 
 </div>
-<Footer/>
-    </div>
    
   );
 };
