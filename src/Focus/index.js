421f9ef7645a7815cfa98cb91745ac14da6d0f49
CloverJiyoon <wldbs8278@gmail.com>
2023-12-04 20:13:39 -0800

    Added Start/End textbox and submit button

--- src/Focus/index.js
+++ src/Focus/index.js
@@ -1,26 +1,53 @@
+import React, { useState } from 'react';
 import Footer from "../Footer/footer";
+import './index.css'; // Import the CSS file for styling
 
 const Focus = () => {
+  const [textBox1Value, setTextBox1Value] = useState('');
+  const [textBox2Value, setTextBox2Value] = useState('');
 
+  const handleTextBox1Change = (event) => {
+    setTextBox1Value(event.target.value);
+  };
 
+  const handleTextBox2Change = (event) => {
+    setTextBox2Value(event.target.value);
+  };
 
-    return(
-        <div>
+  const handleSubmit = () => {
+    // You can perform any actions with the values of the text boxes here
+    console.log('TextBox 1 Value:', textBox1Value);
+    console.log('TextBox 2 Value:', textBox2Value);
+  };
 
-            <h1>Testing Focus</h1>
-            <h2>clover in the house</h2>
+  return (
+    <div className="focus-container">
+      <h1>Testing Focus</h1>
 
+      {/* First text box */}
+      <input
+        type="text"
+        placeholder="Enter Starting Time"
+        value={textBox1Value}
+        onChange={handleTextBox1Change}
+        className="text-box"
+      />
 
+      {/* Second text box */}
+      <input
+        type="text"
+        placeholder="Enter Ending Time"
+        value={textBox2Value}
+        onChange={handleTextBox2Change}
+        className="text-box"
+      />
 
+      {/* Submit button */}
+      <button onClick={handleSubmit} className="submit-button">Submit</button>
 
-
-
-            <Footer/>
-        </div>
-
-
-
-    );
-}
+      <Footer />
+    </div>
+  );
+};
 
 export default Focus;
